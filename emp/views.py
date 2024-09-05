c79adfa3cf1e5c8258b4c80fb1cf021b284104d3
Nikhil26062000 <nikhilsbn06@gmail.com>
2024-09-05 14:33:45 +0530

    App in progress

--- emp/views.py
+++ emp/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from .models import emp
 
 # Create your views here.
+def emp_home(request):
+    # return HttpResponse("Hello This is my new emp app")
+    emps = emp.objects.all
+    return render(request, 'emp/home.html',{'emps':emps})
+
+def add_emp(request):
+    if request.method == 'POST':
+        #fetching the data from form
+        emp_name = request.POST.get('emp_name')
+        emp_id = request.POST.get('emp_id')
+        emp_phone = request.POST.get('emp_phone')
+        emp_address = request.POST.get('emp_address')
+        emp_working = request.POST.get('emp_working')
+        emp_department = request.POST.get('emp_department')
+        #creating the model and set the data
+        e=emp()
+        e.name=emp_name
+        e.emp_id=emp_id
+        e.phone=emp_phone
+        e.working=emp_working
+        e.address=emp_address
+        e.department=emp_department
+        if e.working is None:
+            e.working = False
+        else:
+            e.working = True
+        e.save()
+
+        print('data is coming')
+        return redirect("/emp/home/")
+    return render(request, 'emp/add_emp.html',{})
+
+def delete_emp(request,emp_id):
+    print(emp_id)
+    emps = emp.objects.get(pk=emp_id)
+    emps.delete()
+    return redirect("/emp/home/")
--- firstapp/urls.py
+++ firstapp/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from .views import *
 
 urlpatterns = [
@@ -22,5 +22,6 @@ urlpatterns = [
     path("",home),
     path('index/',home),
     path('about/',about),
-    path('service/',service)
+    path('service/',service),
+    path('emp/',include('emp.urls'))
 ]
