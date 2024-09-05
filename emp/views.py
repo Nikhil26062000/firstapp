e70169f62c879c1cbf50d9465855d1aaf056b42b
Nikhil26062000 <nikhilsbn06@gmail.com>
2024-09-05 15:07:54 +0530

    Employee Management Application

--- emp/views.py
+++ emp/views.py
@@ -40,3 +40,35 @@ def delete_emp(request,emp_id):
     emps = emp.objects.get(pk=emp_id)
     emps.delete()
     return redirect("/emp/home/")
+
+def update_emp(request, emp_id):
+    print(emp_id)
+    emps = emp.objects.get(pk=emp_id)
+    # Pass a dictionary as the context, not a set
+    return render(request, 'emp/updates_emp.html', {'emps': emps})
+
+def do_update_emp(request, emp_id):
+    if request.method == 'POST':
+        emp_name = request.POST.get('emp_name')
+        emp_id_new = request.POST.get('emp_id')
+        emp_phone = request.POST.get('emp_phone')
+        emp_address = request.POST.get('emp_address')
+        emp_working = request.POST.get('emp_working')
+        emp_department = request.POST.get('emp_department')
+        e = emp.objects.get(pk=emp_id)
+        e.name=emp_name
+        e.emp_id=emp_id_new
+        e.phone=emp_phone
+        e.working=emp_working
+        e.address=emp_address
+        e.department=emp_department
+        if e.working is None:
+            e.working = False
+        else:
+            e.working = True
+        e.save()
+
+
+
+        
+    return redirect("/emp/home")
